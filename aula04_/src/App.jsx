f574ef2a2fd9c5676b8c1cb25b07e44813753783
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2024-09-19 14:15:00 -0300

    Criando estrutura React
    Estrutura usada no curso de react, removendo itens desnecessários

---
+++ aula04_/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+
+  return (
+    <>
+       <a className="ativo" href="www.google.com">Teste</a>
+      <label htmlFor="nome">Nome</label>
+      <input type="text" id="nome" />
+    </>
+     
+  )
+};
+
+export default App
