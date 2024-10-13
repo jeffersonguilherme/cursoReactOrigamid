0427fd566b07a028f135033adf54cde4f8dd5fed
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2024-10-13 18:11:11 -0300

    Aula 05
    JSX  e exercicio de DOM

--- aula05 - vite/src/App.jsx
+++ aula05 - vite/src/App.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 
+const titulo = <h1>esse é um titulo</h1>
+
 const App = () => {
 
-  return (
-      <div>
-       App React <button>Adicionar</button>
-    </div>
-  )
-}
+  const random = Math.random();
+  const ativo = false;
 
-export default App
+  function mostrarNome(sobrenome){
+    return 'Jefferson' + sobrenome;
+  }
+
+  const carro = {
+    marca : "Forde",
+    rodas: '4',
+  };
+
+  const estiloP = {
+    color: 'blue',
+    fontSize: '2rem',
+  };
+
+  return(
+    <>
+      {titulo}
+      <p>{true ? "verdadeiro" : "falso"} - {10 }
+        { mostrarNome('Gui')}
+      </p>
+
+      <p style={estiloP}>{new Date().getFullYear()}</p>
+      <p>{carro.marca}</p>
+      <p>{carro.rodas}</p>
+      <p className={ativo ? 'ativo' : 'inativo'}>{random * 100}</p>
+
+
+
+    </>
+    
+  );
+};
+ 
+export default App;
