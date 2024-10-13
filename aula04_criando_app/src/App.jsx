56fb9a159647a61878866dbff585e7df91d32cc1
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2024-10-12 21:00:27 -0300

    Aula 05
    Criando Aplicação react com Vite
    Configuração do ambiente de desevolvimento

---
+++ aula04_criando_app/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () =>{
+    return <a href="www.google.com">Google</a>
+}
+
+
+export default App;
---
+++ aula05 - vite/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+
+  return (
+      <div>
+       App React <button>Adicionar</button>
+    </div>
+  )
+}
+
+export default App
